4c0c2ee79bdf87673425a2e4edb090cc52682798
Sara Ayala Carrillo <sayala-c@cbr2s3.42barcelona.com>
2026-05-18 13:24:51 +0200

    01

---
+++ ft_garden_intro.py
@@ -0,0 +1,18 @@
+def welcome():
+    print("=== Welcome to My Garden ===")
+
+
+def end():
+    print("=== End of Program ===")
+
+
+def ft_garden_intro():
+    print("Plant: Rose")
+    print("Height: 25cm")
+    print("Age: 30 days\n")
+
+
+if __name__ == "__main__":
+    welcome()
+    ft_garden_intro()
+    end()
