dff1ade64687baffb7887a52ea22bb2801ffdafa
Sara Ayala Carrillo <sayala-c@cbr3s5.42barcelona.com>
2026-05-20 20:04:38 +0200

    update

---
+++ ex2/ft_plant_growth.py
@@ -0,0 +1,8 @@
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f"{self.name}: {self.height} cm, {self.age} days old")
